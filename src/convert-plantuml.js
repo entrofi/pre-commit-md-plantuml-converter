6e6108a8b889b815c096fd5f67578ec9aff7f9e2
Hasan Çomak <entrofi@gmail.com>
2022-08-14 07:48:53 +0200

    #4: Replace docopts with commander
    -Docopts replaced with the commander lib
    -Changed the node cli to accept filename instead of the content
    All file read/write processing is delegated to nodejs.

--- src/convert-plantuml.js
+++ src/convert-plantuml.js
@@ -1,20 +1,8 @@
 #!/usr/bin/env node
+const {Command} = require('commander');
+const fs = require('fs');
 const PlantumlSnippetConverter = require('../src/plantuml-snippet-converter').PlantumlSnippetConverter;
 const MdPumlMatchers = require('./md-puml-matchers');
-const doc = `
-Usage:
-  convert-plantuml.js <markdownWithPlantuml> <extension> <markdownFilePath> [<imagePath>]  [<imagePrefix>]
-Arguments:
-  plantuUmlComment      The markdown comment string containing plantuml markup \`\`\`plantum \`\`\`
-  extension             Image type extension e.g.: 'svg', 'png', 'jpg'
-  markdownFilePath      Relative path of the current markdown file
-  imagePath             Save path for the generated images
-  imagePrefix           A prefix to add to generated image filenames
-Example:
-  convert-plantuml.js "# Hello world" "png" "prefix_"
-`;
-
-const docopt = require('docopt').docopt;
 
 
 const convertPumlMarkup = (mdPumlSnippet, extension, markdownFilePath, imagePrefix, imagePath) => {
@@ -23,16 +11,36 @@ const convertPumlMarkup = (mdPumlSnippet, extension, markdownFilePath, imagePref
   return mdPumlConverter.convert();
 };
 
-const main = async (config) => {
-  const markdown = config['<markdownWithPlantuml>'];
-  const extension = config['<extension>'];
-  const prefix = config['<imagePrefix>'];
-  const imagePath = config['<imagePath>'];
-  const markdownFilePath = config['<markdownFilePath>'];
-  const pumlMdSnippetMatch = MdPumlMatchers.resetAndGetMarkdownPlantumlSnippetMatcher();
-  const reformattedMarkdown = markdown.replace(pumlMdSnippetMatch,
-      (s) => convertPumlMarkup(s, extension, markdownFilePath, prefix, imagePath));
-  return reformattedMarkdown;
-};
+const mdPumlConverterCommand = new Command();
 
-main(docopt(doc)).then(console.log).catch(console.error);
+mdPumlConverterCommand
+    .command('run')
+    .argument('<markdownFile>', 'The markdown file which contains plantuml markups\n' +
+        'in the following form ```plantuml \n ....```')
+    .requiredOption('-e, --extension <extension>', 'Image type extension e.g.: \'svg\', \'png\' (default), \'jpg\'')
+    .requiredOption('-f, --markdown-file-path <markdownFilePath>', 'Relative path of the current markdown file')
+    .option('-i, --image-path [imagePath]', 'Save path for the generated images')
+    .option('-p, --image-prefix [imagePrefix]', 'A prefix to add to generated image filenames')
+    .option('-o, --output-file [outputFile]', 'output file to write converted markdown. Default is empty' )
+    .action(function(markdownFile) {
+      const readFile = () => {
+        return fs.readFileSync(markdownFile, 'utf-8')
+            .toString();
+      };
+      const markdown = readFile();
+      const options = this.opts();
+      const extension = options.extension;
+      const prefix = options.imagePath;
+      const imagePath = options.imagePrefix;
+      const markdownFilePath = options.markdownFilePath;
+      const pumlMdSnippetMatch = MdPumlMatchers.resetAndGetMarkdownPlantumlSnippetMatcher();
+      const processedOutput = markdown.replace(pumlMdSnippetMatch,
+          (s) => convertPumlMarkup(s, extension, markdownFilePath, prefix, imagePath));
+      const outputFile = options.outputFile ? options.outputFile : markdownFile;
+      fs.writeFileSync(outputFile, processedOutput);
+    });
+
+
+mdPumlConverterCommand.parse();
+
+
